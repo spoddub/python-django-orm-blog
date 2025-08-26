61ccf379c1b431feeaac3de3863606cc8869148e
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-08-26 19:09:44 +0300

    add nickname option

--- python_django_orm_blog/blog/models.py
+++ python_django_orm_blog/blog/models.py
@@ -17,6 +17,7 @@ class User(TimestampedModel):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
+    nickname = models.CharField(max_length=100, null=True)
 
 
 class Tag(TimestampedModel):
